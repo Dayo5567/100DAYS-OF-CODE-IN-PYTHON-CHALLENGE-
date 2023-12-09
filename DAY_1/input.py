ade309ce7402311b51cdc07ac75b5c8c6f78ccd0
hammed <hammedoyinkansola123@gmail.com>
2023-12-09 11:37:53 +0100

    day 1

--- DAY_1/input.py
+++ DAY_1/input.py
@@ -1,3 +1,6 @@
 # input() will get user input in console
 # then print() will print the word "Hello" and the user input
-print("Hello " + input("what is your name?") + "!")
+print("Hello " + input("what is your name? ") + "!")
+
+#printing the length of a character
+print(len(input("what is your name? ")))
---
+++ DAY_1/var.py
@@ -0,0 +1,13 @@
+name = input("what is name? ")
+print(name)
+
+
+##########
+# get the length of the name directly
+name = input("what is name? ")
+length = len(name)
+print(length)
+
+#####
+name = "Jack"
+print(name)
